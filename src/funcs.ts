ab551504c7397ff235e0969da9e5dc85cb5c56a9
akimov.infotech <akimov.infotech@gmail.com>
2019-03-26 15:54:29 +0700

    using samples from book

---
+++ src/funcs.ts
@@ -0,0 +1,11 @@
+// let myFunc = function () {
+//     console.log("This is a statement");
+// };
+// myFunc();
+
+export class Name {
+    myFunc() {
+        console.log("This is a myFunc function call statement");
+    }
+};
+
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
+import  { Name } from './funcs';
+
+let name = new Name();
+name.myFunc();
 console.log('Hello');
+console.log("Apples");
